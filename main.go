9b7023cc3eb0987496cab15dae2b47ccb642aff6
nicetas.levchnko@gmail.com <90518478+nicetasL@users.noreply.github.com>
2024-07-12 11:47:11 +0300

    [feat] Add basic handler for root route

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
+
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to Notes API")
+	})
+	http.ListenAndServe(":8080", nil)
+}
